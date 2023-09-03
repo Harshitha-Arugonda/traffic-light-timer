6e6be0edfe557a5e2739e8ba4a1d344ae152f69e
Harshitha-Arugonda <116995616+Harshitha-Arugonda@users.noreply.github.com>
2023-09-03 12:20:48 +0530

    Update dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -5,26 +5,32 @@ import pandas as pd
 n = 1000  # Change this to your desired number of rows
 
 # Define the min and max values for each feature
-f1_min = 0
+f1_min = 1
 f1_max = 10
-f2_min = 0
-f2_max = 5
-f3_min = -1
-f3_max = 1
-f4_min = -5
-f4_max = 5
+f2_min = 1
+f2_max = 30
+f3_min = 1
+f3_max = 7
+f4_min = 1
+f4_max = 4
+f5_min =18
+f5_max =60
+f6_min =1
+f6_max =3
 
 # Define the weights for each feature
 w1 = 0.5
 w2 = 0.3
 w3 = -0.2
 w4 = 0.1
+w5 = 0.4
+w6 = 0.2
 
 # Define the threshold for assigning classes
-threshold = 2.0
+threshold = (((f1_max*w1+f2_max*w2+f3_max*w3+f4_max*w4+f5_max*w5+f6_max*w6)*3)/4)
 
 # Initialize lists to store the data
-data = {'f1': [], 'f2': [], 'f3': [], 'f4': [], 'outcome': [], 'class': []}
+data = {'distance': [], 'no vehc': [], 'day': [], 'time': [], 'age': [], 'purpose': [],'outcome':[]}
 
 # Generate n rows of data
 for _ in range(n):
@@ -33,26 +39,22 @@ for _ in range(n):
     f2 = int(random.uniform(f2_min, f2_max))
     f3 = int(random.uniform(f3_min, f3_max))
     f4 = int(random.uniform(f4_min, f4_max))
-
+    f5 = int(random.uniform(f5_min, f5_max))
+    f6 = int(random.uniform(f6_min, f6_max))
     # Calculate the outcome based on the weights
-    outcome = f1 * w1 + f2 * w2 + f3 * w3 + f4 * w4
-
-    # Assign a class based on the outcome and threshold
-    if outcome >= threshold:
-        class_label = 1
-    else:
-        class_label = 0
+    outcome = f1 * w1 + f2 * w2 + f3 * w3 + f4 * w4 + f5 * w5 + f6 * w6
 
     # Append the data to the lists
-    data['f1'].append(f1)
-    data['f2'].append(f2)
-    data['f3'].append(f3)
-    data['f4'].append(f4)
+    data['distance'].append(f1)
+    data['no vehc'].append(f2)
+    data['day'].append(f3)
+    data['time'].append(f4)
+    data['age'].append(f5)
+    data['purpose'].append(f6)
     data['outcome'].append(outcome)
-    data['class'].append(class_label)
 
 # Create a DataFrame from the data
 df = pd.DataFrame(data)
 
 # Save the DataFrame to a CSV file
-df.to_csv('synthetic_dataset.csv', index=False)
+df.to_csv('traffic_dataset.csv', index=False)
